2b1fbe91a10a088f6bca575bbccc62292862c0e6
Sandu Ursu <sandu.ursu@gmail.com>
2018-11-15 12:58:48 +0100

    Workshop

---
+++ Workshop/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+   name='Workshop', 
+   version='0.1',
+   packages=['workshop',],
+   license='MIT', 
+   # long_description=open('README.txt').read(),
+)
